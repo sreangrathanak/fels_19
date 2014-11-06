e6bfee189f5574eb7b0a292a0e243896367e68e7
sreangrathanak <sreangrathanak@yahoo.com>
2014-11-07 06:07:25 +0700

    Seed Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,51 @@ User.create!(name:  "Rathanak Jame",
                password:              password,
                password_confirmation: password)
 end
+#declare users variable
+users = User.order(:created_at).take(6)
+
+#Category
 10.times do |n|
   name="Rathanak-#{n+1} Category"
   Category.create(name: name)
+end
+#declare users variable
+categories = Category.order(:created_at).take(3)
+# get only one user
+user_first=users.first
+#Lesson
+5.times do
+  result="wrong-right"
+  categories.each{|category|
+    Lesson.create(result: result,user_id: user_first.id, category_id: category.id)
+ }
+end
+#Word 
+3.times do |n|
+  content="Test-#{n+1}"
+  categories.each{|category|
+    Word.create(content: content,category_id:category.id)
+  }
+end
+#WordAnswer
+words=Word.order(:created_at)
+3.times do |n|
+  content="Test Answer-#{n+1}"
+  words.each{|word|
+    WordAnswer.create(content: content,word_id:word.id)
+  }
+end
+#get lesson
+lessons= Lesson.order(:created_at).take(1)
+#get word answer
+word_answers=WordAnswer.order(:created_at)
+#LessonWord
+2.times do
+lessons.each{|lesson|
+  words.each{|word|
+    word_answers.each{|word_answer|
+      LessonWord.create(lesson_id: lesson.id,word_id:word.id,word_answer_id:word_answer.id)
+    }
+  }
+}
 end
