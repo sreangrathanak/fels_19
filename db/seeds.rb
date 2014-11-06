957e0e30b2fa0ec6439991ea4c357c7b2e00d568
sreangrathanak <sreangrathanak@yahoo.com>
2014-11-06 17:58:10 +0700

    Finish User Page( Login, Log out, Show user, Update, Delete User)

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+
+User.create!(name:  "Rathanak Jame",
+             email: "sreangrathanak@gmail.com",
+             password:              "password",
+             password_confirmation: "password",
+             isadmin: true)
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
